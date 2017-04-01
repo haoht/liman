f78fe4b14f714ab76db84605935e329651118a08
mertcelen <mcelen94@gmail.com>
2017-04-01 22:32:26 +0300

    Initial update of Liman with Python rebase.

---
+++ bin/liman.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+import os
+import sys
+import shutil
+
+
+def handler(action, name):
+
+    if action == 'list':
+        listall()
+    elif action == 'update':
+        update()
+    elif action == 'repos':
+        repositories()
+    elif action == 'add':
+            add(name)
+    elif action == 'remove':
+        remove(name)
+    elif action == 'run':
+        run(name)
+    else:
+        print('Command not found')
+
+
+def add(name):
+    # Creating repository folder with right permissions
+    os.system('mkdir -p /usr/local/liman2/' + str(name))
+    os.chdir('/usr/local/liman2/' + str(name))
+
+    # Initializing git and enabling sparse checkout
+    os.system('git init && git remote add origin https://github.com/' + str(name.replace('#', '/')) + '.git ')
+    os.system('echo \'scripts/*\' >> .git/info/sparse-checkout && git config core.sparsecheckout true ')
+    print(str(name) + ' added!')
+
+    # Finally update
+    update()
+
+
+def update():
+    # First get list of repositories initialized
+    repos = os.listdir('/usr/local/liman2')
+
+    for current in repos:
+        # Updating repository with sparsecheckout
+        os.chdir('/usr/local/liman2/' + str(current))
+        os.system('git pull --depth=2 origin master')
+        os.chmod('/usr/local/liman2/' + str(current) + '/scripts/', 744)
+        print(str(current) + ' updated!')
+    print('All repositories updated.')
+
+
+def remove(name):
+    # Simply removing folder
+    name = name.replace('/', '#')
+    shutil.rmtree('/usr/local/liman2/' + str(name))
+    print(str(name) + ' removed!')
+
+
+def listall():
+    # Listing files in all directories (repositories) under liman2.
+    os.chdir('/usr/local/liman2/')
+    os.system('find . -iname *.sh')
+
+
+def run(name):
+    # Basic if statement to add .sh extension to the name
+    name = str(name) + '.sh' if not name.endswith('.sh') else name
+
+    # Give permission to run the script
+    os.chmod('/usr/local/liman2/liman2#liman2-depo/scripts/' + str(name), 100)
+    os.system('bash /usr/local/liman2/liman2#liman2-depo/scripts/' + str(name))
+
+
+def repositories():
+    repos = os.listdir('/usr/local/liman2')
+
+    for current in repos:
+        print(current)
+
+
+if len(sys.argv) > 2:
+    handler(sys.argv[1], str(sys.argv[2]).replace('/', '#'))
+elif len(sys.argv) > 1:
+    handler(sys.argv[1], '')
+else:
+    print('Please deploy an action')
+
+
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Simple installer for Liman Script Manager
+#Still in early development
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+cp bin/liman.py /usr/bin/liman
+chmod +x /usr/bin/liman
+mkdir -p /usr/local/liman/
+
+echo "Let's install default repositories as well"
+liman add liman/liman-depo
+#clear
+echo "Liman installed, default repository added"
